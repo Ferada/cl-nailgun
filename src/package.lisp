5724cfda7db2755633036ccc78352f748f95d649
Olof-Joachim Frahm <olof@macrolet.net>
2014-02-27 20:01:00 +0100

    Export main function.

--- src/package.lisp
+++ src/package.lisp
@@ -6,4 +6,6 @@
   (:use #:cl #:iolib #:trivial-gray-streams #:bordeaux-threads #:alexandria #:flexi-streams)
   (:import-from #:arnesi #:with-collector)
   (:import-from #:nibbles #:ub32ref/be #:write-ub32/be #:make-octet-vector)
-  (:shadowing-import-from #:babel #:octets-to-string))
+  (:shadowing-import-from #:babel #:octets-to-string)
+  (:export ;;; main entry point
+           #:run-server))
