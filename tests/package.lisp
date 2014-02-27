809612fdfc748293d0623040e6417793c202214f
Olof-Joachim Frahm <olof@macrolet.net>
2014-02-27 19:59:33 +0100

    Tests infrastructure.

---
+++ tests/package.lisp
@@ -0,0 +1,6 @@
+;;; -*- mode: lisp; syntax: common-lisp; coding: utf-8-unix; package: cl-user; -*-
+
+(in-package #:cl-user)
+
+(defpackage #:cl-nailgun-tests
+  (:use #:cl #:cl-nailgun #:fiveam))
---
+++ tests/suite.lisp
@@ -0,0 +1,5 @@
+;;; -*- mode: lisp; syntax: common-lisp; coding: utf-8-unix; package: cl-nailgun-tests; -*-
+
+(in-package #:cl-nailgun-tests)
+
+(def-suite cl-nailgun)
